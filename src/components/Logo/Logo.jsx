135febe77e044d5e56ab5caabeb19f1209666d45
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-07-19 17:06:34 +0300

    feat: добавить роут для logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,12 @@
 import './Logo.css';
 import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Logo({ position }) {
     return (
-        <img className={`logo ${position}`} src={logo} alt="Логотип сайта" />
+        <Link to="/">
+            <img className={`logo ${position}`} src={logo} alt="Логотип сайта" />
+        </Link>
+
     )
 }
