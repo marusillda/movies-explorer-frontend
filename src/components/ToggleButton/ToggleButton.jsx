e5f279b96cd16d99b729315e358ca0f218ab7ffb
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-07-23 15:20:26 +0300

    fix: добавить состояния наведения кнопкам

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -4,7 +4,7 @@ export default function ToggleButton({ text }) {
     return (
         <div className="togglebutton">
             <input className="togglebutton__input" type="checkbox" id="switch" />
-            <label className="togglebutton__label" htmlFor="switch" />
+            <label className="togglebutton__label  selectable-button" htmlFor="switch" />
             <p className="togglebutton__text">{text}</p>
         </div>
     )
