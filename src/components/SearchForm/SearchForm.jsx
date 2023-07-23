5c46dec44c6f0a23410643f58476022c5736b515
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-07-23 14:58:39 +0300

    fix: позиционировать searchform__submit-button

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,22 +5,20 @@ export default function SearchForm() {
     return (
         <section className="searchform" aria-label="Форма поиска">
             <form className="searchform__container">
-                <div className="searchform__field">
-                    <input
-                        className="searchform__field-movie"
-                        type="text"
-                        id="movie"
-                        name="movie"
-                        placeholder="Фильм"
-                    />
-                    <button
-                        type="submit"
-                        className="searchform__submit-button selectable-button"
-                        aria-label="Кнопка поиска фильма"
-                    >
-                        Поиск
-                    </button>
-                </div>
+                <input
+                    className="searchform__field-movie"
+                    type="text"
+                    id="movie"
+                    name="movie"
+                    placeholder="Фильм"
+                />
+                <button
+                    type="submit"
+                    className="searchform__submit-button selectable-button"
+                    aria-label="Кнопка поиска фильма"
+                >
+                    Поиск
+                </button>
             </form>
             <ToggleButton text="Короткометражки" />
         </section>
